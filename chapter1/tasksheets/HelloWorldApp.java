86df0a84ad3e7e6a0fa67e8b9f248d8608894b3b
Chris Rivera <chrislawrencerivera6@gmail.com>
2024-10-29 11:10:41 +0000

    1st Task HelloWorldApp

---
+++ chapter1/tasksheets/HelloWorldApp.java
@@ -0,0 +1,10 @@
+package chapter1.tasksheets;
+
+public class HelloWorldApp {
+    String name;
+
+    public HelloWorldApp() {
+        name = "Komusta Mundo!";
+
+    }
+}
---
+++ chapter1/tasksheets/Main.java
@@ -0,0 +1,9 @@
+package chapter1.tasksheets;
+
+public class Main {
+
+    public static void main(String[] args) {
+        HelloWorldApp newWord = new HelloWorldApp();
+        System.out.println(newWord.name);
+    }
+}
