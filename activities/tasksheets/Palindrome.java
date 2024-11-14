7ac49d691bf3fb1f669c28e7cfc1f1221ff8f10d
Chris Rivera <chrislawrencerivera6@gmail.com>
2024-11-14 01:42:53 +0000

    change code in simplest code

--- activities/tasksheets/Palindrome.java
+++ activities/tasksheets/Palindrome.java
@@ -2,22 +2,39 @@ package activities.tasksheets;
 
 public class Palindrome {
     public static void main(String[] args) {
-        String input = "madam";
-        char charInput[] = { 'm', 'a', 'm' };
 
-        if (isPalindrome(input)) {
-            System.out.println("The " + input + " is Palindrome");
-        } else {
-            System.out.println("The " + input + " is not Palindrome");
-        }
-
-        if (isPalindrome(charInput)) {
-            System.out.println("The " + (new String(charInput)) + " is Palindrome");
-        } else {
-            System.out.println("The " + (new String(charInput)) + " is not Palindrome");
-        }
+String input = "madam";
+        System.out.println(checkPalindrome(input)?  input +" is Palindrome" : input + " is not Palindrome");
+      
     }
- // public static boolean isPalindrome(String str) {
+
+    public static boolean checkPalindrome(String strInput){
+
+        for(int x=0; x<strInput.length()/2; x++){
+            if(strInput.charAt(x) != strInput.charAt(strInput.length() -x -1)){
+                return false;
+            }
+        }
+        return true;
+
+    }
+
+        // String input = "madam";
+        // char charInput[] = { 'm', 'a', 'm' };
+
+        // if (isPalindrome(input)) {
+        // System.out.println("The " + input + " is Palindrome");
+        // } else {
+        // System.out.println("The " + input + " is not Palindrome");
+        // }
+
+        // if (isPalindrome(charInput)) {
+        // System.out.println("The " + (new String(charInput)) + " is Palindrome");
+        // } else {
+        // System.out.println("The " + (new String(charInput)) + " is not Palindrome");
+        // }
+   
+    // public static boolean isPalindrome(String str) {
     // int length = str.length();
 
     // for (int i = 0; i < length; i++) {
@@ -27,31 +44,29 @@ public class Palindrome {
     // }
     // return true;
     // }
-    public static boolean isPalindrome(String input) {
-        char arrayInput[] = input.toCharArray();
-        for (int x = 0; x < arrayInput.length; x++) {
+    // public static boolean isPalindrome(String input) {
+    // char arrayInput[] = input.toCharArray();
+    // for (int x = 0; x < arrayInput.length; x++) {
 
-            if (arrayInput[x] != arrayInput[arrayInput.length - x - 1]) {
-                return false;
-            }
+    // if (arrayInput[x] != arrayInput[arrayInput.length - x - 1]) {
+    // return false;
+    // }
 
-        }
-        return true;
+    // }
+    // return true;
 
-    }
+    // }
 
-    public static boolean isPalindrome(char input[]) {
-        int length = input.length;
-        for (int x = 0; x < length; x++) {
+    // public static boolean isPalindrome(char input[]) {
+    // int length = input.length;
+    // for (int x = 0; x < length; x++) {
 
-            if (input[x] != input[input.length - x - 1]) {
-                return false;
-            }
+    // if (input[x] != input[input.length - x - 1]) {
+    // return false;
+    // }
 
-        }
-        return true;
-    }
-
-   
+    // }
+    // return true;
+    // }
 
 }
