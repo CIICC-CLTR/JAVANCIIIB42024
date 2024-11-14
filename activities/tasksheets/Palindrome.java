0703a4bb13f8f830c6155c875dc67f548b59a264
Chris Rivera <chrislawrencerivera6@gmail.com>
2024-11-14 01:49:35 +0000

    change code into simplest way

--- activities/tasksheets/Palindrome.java
+++ activities/tasksheets/Palindrome.java
@@ -3,37 +3,35 @@ package activities.tasksheets;
 public class Palindrome {
     public static void main(String[] args) {
 
-String input = "madam";
-        System.out.println(checkPalindrome(input)?  input +" is Palindrome" : input + " is not Palindrome");
-      
+        String input = "madam";
+        System.out.println(checkPalindrome(input) ? input + " is Palindrome" : input + " is not Palindrome");
+
     }
 
-    public static boolean checkPalindrome(String strInput){
+    public static boolean checkPalindrome(String strInput) {
 
-        for(int x=0; x<strInput.length()/2; x++){
-            if(strInput.charAt(x) != strInput.charAt(strInput.length() -x -1)){
-                return false;
-            }
+        for (int x = 0; x < strInput.length() / 2; x++) {
+            if (strInput.charAt(x) != strInput.charAt(strInput.length() - x - 1)) {return false;}
         }
         return true;
 
     }
 
-        // String input = "madam";
-        // char charInput[] = { 'm', 'a', 'm' };
+    // String input = "madam";
+    // char charInput[] = { 'm', 'a', 'm' };
 
-        // if (isPalindrome(input)) {
-        // System.out.println("The " + input + " is Palindrome");
-        // } else {
-        // System.out.println("The " + input + " is not Palindrome");
-        // }
+    // if (isPalindrome(input)) {
+    // System.out.println("The " + input + " is Palindrome");
+    // } else {
+    // System.out.println("The " + input + " is not Palindrome");
+    // }
 
-        // if (isPalindrome(charInput)) {
-        // System.out.println("The " + (new String(charInput)) + " is Palindrome");
-        // } else {
-        // System.out.println("The " + (new String(charInput)) + " is not Palindrome");
-        // }
-   
+    // if (isPalindrome(charInput)) {
+    // System.out.println("The " + (new String(charInput)) + " is Palindrome");
+    // } else {
+    // System.out.println("The " + (new String(charInput)) + " is not Palindrome");
+    // }
+
     // public static boolean isPalindrome(String str) {
     // int length = str.length();
 
