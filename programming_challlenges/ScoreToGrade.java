f592a10c9c46e60f897c361ccb8efb7958c83239
Chris Rivera <chrislawrencerivera6@gmail.com>
2024-11-15 12:24:47 +0000

    add validation

--- programming_challlenges/ScoreToGrade.java
+++ programming_challlenges/ScoreToGrade.java
@@ -1,23 +1,50 @@
 package programming_challlenges;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ScoreToGrade {
+    public static Scanner scan = new Scanner(System.in);
+
     public static void main(String[] args) {
-        double grade = 0.0;
 
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Enter Your Grade: ");
-        grade = scan.nextDouble();
+        System.out.println(new ScoreToGrade().getGradeequivalent("\nEnter Grade: "));
 
-        if (grade <= 100 && grade >= 90){System.out.println("Your " + grade + " grade is eqivalent of A");}
-        else if (grade <= 89 && grade >= 80){System.out.println("Your " + grade + " grade is eqivalent of B");}
-        else if (grade <= 79 && grade >= 70){System.out.println("Your " + grade + " grade is eqivalent of C");}
-        else if (grade <= 69 && grade >= 60){System.out.println("Your " + grade + " grade is eqivalent of D");}
-        else if (grade < 60){System.out.println("Your " + grade + " grade is eqivalent of F");}
-        else{System.out.println("Please Input Valid Grade");}
-            
-            
     }
 
+    public String getGradeequivalent(String prompt){
+
+      double grade =  checkValidnumber(prompt);
+       String msg="";
+       
+        if (grade <= 100 && grade >= 90){msg = "Your " + grade + " grade is eqivalent of A";}
+        else if (grade <= 89 && grade >= 80){msg = "Your " + grade + " grade is eqivalent of B";}
+        else if (grade <= 79 && grade >= 70){msg = "Your " + grade + " grade is eqivalent of C";}
+        else if (grade <= 69 && grade >= 60){msg = "Your " + grade + " grade is eqivalent of D";}
+        else if (grade < 60){msg = "Your " + grade + " grade is eqivalent of F";}
+        else {msg= "Please Input Valid Grade";}
+        return msg;   
+    }
+       public double checkValidnumber(String prompt) {
+         
+        double grade = 0.0;
+
+        while (true) {
+            System.out.print(prompt);
+            try {
+                grade =scan.nextDouble() ;
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input! Accepting Integer only!");
+                scan.nextLine();
+            }
+
+        }
+
+        return grade;
+
+    }
+
+
+    
 }
