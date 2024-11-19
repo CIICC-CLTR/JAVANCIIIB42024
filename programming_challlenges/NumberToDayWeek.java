870606caeb4934f69d4f6574bce263722b6da2ea
Chris Rivera <chrislawrencerivera6@gmail.com>
2024-11-19 05:57:35 +0000

    add validation

--- programming_challlenges/NumberToDayWeek.java
+++ programming_challlenges/NumberToDayWeek.java
@@ -1,45 +1,52 @@
 package programming_challlenges;
+
 import java.util.Scanner;
+
 public class NumberToDayWeek {
-    
+
     public static void main(String[] args) {
-    Scanner scan = new Scanner(System.in);
+     
+        System.out.println(new NumberToDayWeek().toDay());
 
-    double number = 0.0;
-
-    System.out.print("\nEnter numbers [1 to 7] to convert for Day in a Week: ");
-    number = scan.nextDouble();
-
-    System.out.println(toDay(number));
-
-   
     }
 
-    public static String toDay(double num){
-        String day=""; int n = (int)num;
-
-        if ((isValidChoice(num))) {
-            switch ((int)num) {
-                case 1: day = n + " is equivalent to Monday\n";break;
-                case 2: day = n + " is equivalent to Tuesday\n";break;
-                case 3: day = n + " is equivalent to Wednesday\n";break;
-                case 4: day = n + " is equivalent to Thursday\n";break;
-                case 5: day = n + " is equivalent to Friday\n";break;
-                case 6: day = n + " is equivalent to Saturday\n";break;
-                case 7: day = n + " is equivalent to Sunday\n";break;
-                default: day = n + "is invalid input\n";break;
+    public String toDay() {
+        String day = "";
+        int n = new NumberToDayWeek().userPrompt();
+ 
+        if ((isValidChoice(n))) {
+            switch (n) {
+                case 1: day = n + " is equivalent to Monday\n"; break;
+                case 2: day = n + " is equivalent to Tuesday\n"; break;
+                case 3: day = n + " is equivalent to Wednesday\n"; break;
+                case 4: day = n + " is equivalent to Thursday\n"; break;
+                case 5: day = n + " is equivalent to Friday\n"; break;
+                case 6: day = n + " is equivalent to Saturday\n"; break;
+                case 7: day = n + " is equivalent to Sunday\n"; break;
+                default: day =  "You entered invalid input\n"; break;
             }
-            
         }
         return day;
     }
 
-    public static boolean isValidChoice(double num){
-
-      if((int) num<=7 || (int) num>=1)
-
-        return true;
+    public int userPrompt(){
+        Scanner scan = new Scanner(System.in);
+        double number=0.0;
+        System.out.print("\nEnter number [1 t0 7] to convert into Day in a Week: ");
     
-    return false;
-}
+        try {
+            number = scan.nextDouble();
+            return (int)number;
+        } catch (Exception e) {
+            return (int)number;
+        }
+     
+    }
+
+    public static boolean isValidChoice(int num) {
+
+        if ( num <= 7 ||  num >= 1)
+           return true;
+           return false;
+    }
 }
