005a012167c790a9bf6e5fbd2915b2175a4a290c
Chris Rivera <chrislawrencerivera6@gmail.com>
2024-11-21 14:15:29 +0000

    b

--- programming_challlenges/Count_Word_In_Input.java
+++ programming_challlenges/Count_Word_In_Input.java
@@ -1,20 +1,18 @@
 package programming_challlenges;
-import java.util.List;
 import java.util.Scanner;
 
 public class Count_Word_In_Input {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        List[] words;
+        String[] words;
         
         System.out.print("\nEnter a sentence: ");
         String input = scan.nextLine();
 
-        System.out.println((words = new List[(input.split("\\s")).length]).length +" - is the word count in the word: " + input +"\n");
+        System.out.println((words = new String[(input.split("\\s")).length]).length +" - is the word count in the word: " + input +"\n");
 
         scan.close();
     }
     
 }
-
-         
+ 
