3d9e39436235db98f8ccbb74edfc6108ddb5a355
Chris Rivera <chrislawrencerivera6@gmail.com>
2024-11-21 12:59:25 +0000

    cngae some code

--- programming_challlenges/String_Builder_10_Characters.java
+++ programming_challlenges/String_Builder_10_Characters.java
@@ -16,22 +16,27 @@ public class String_Builder_10_Characters {
      
 
         StringBuilder sbInput = new StringBuilder(input.toLowerCase());
-        StringBuilder temp1 = new StringBuilder();
+        String temp2 = "";
       char temp;
 
         if(checkCharacterLength(input)){
             
-            System.out.println((sbInput.charAt(0)) +" - is The Fisrt index of the word: " + sbInput );
+            System.out.println("\n"+(sbInput.charAt(0)) +" - is The Fisrt index of the word: " + sbInput );
             System.out.println(sbInput.charAt(sbInput.length()-1) +" - is The Last index of the word: " + sbInput );
             System.out.println((occurance=sbInput.indexOf("a"))!= -1? occurance + " - is the index of first occurence of letter a of the word " +sbInput:"Occurence is not found");
             System.out.println(sbInput.substring(3, 6) +" - is the substring of  " + sbInput +" from index 3 to index 6");
             System.out.println(sbInput.append("123") +" - is the appended word with 123 string at the end of the word");
-            System.out.println(sbInput.insert(4, "xyz") +"- is the inserted with xyz at index 4");
-            System.out.println(sbInput.delete(2, 4)+" - has a deleted substring from index 2 to index 4    ");
-            temp1 =sbInput;
-           temp = sbInput.charAt(8);
-           sbInput.deleteCharAt(8);
-            System.out.println(temp + " was deleted from the word " + temp1 + " at index 8 " );
+            System.out.println(sbInput.insert(4, "xyz") +"- has an inserted xyz at index 4");
+            temp2 =sbInput.toString();
+            System.out.println(sbInput.delete(2, 4)+" - has a deleted substring from index 2 to index 4 from the word: " +temp2);
+           
+            temp2 =sbInput.toString();
+            temp = sbInput.charAt(8);
+            sbInput.deleteCharAt(8);
+           
+            System.out.println(temp + " - character was deleted from the word " + temp2 + " at index 8 " );
+           temp2 =sbInput.toString();
+            System.out.println(sbInput.reverse().toString()+" - was reversed from the word: " + temp2 +"\n");
 
         }
         else{
